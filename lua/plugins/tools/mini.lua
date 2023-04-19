1b46c6e7ed23d267c3ef137c9d47fadc61d2296c
yqwu905 <pingline@outlook.com>
2023-04-20 01:10:43 +0800

    plugins: remove some components of mini.nvim

--- lua/plugins/tools/mini.lua
+++ lua/plugins/tools/mini.lua
@@ -4,25 +4,14 @@ return {
   lazy = false,
   config = function()
     require("mini.surround").setup()
-    require("mini.ai").setup()
-    require("mini.cursorword").setup()
     require("mini.pairs").setup()
     require("mini.comment").setup()
     require("mini.statusline").setup()
-    require("mini.jump").setup()
     require("mini.trailspace").setup()
     local indentscope = require "mini.indentscope"
     indentscope.setup {
       draw = {
         animation = indentscope.gen_animation.none(),
-      },
-    }
-    require("mini.sessions").setup {
-      autowrite = true,
-    }
-    require("mini.jump2d").setup {
-      mappings = {
-        start_jumping = "s",
       },
     }
   end,
